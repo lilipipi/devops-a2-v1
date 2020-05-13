86f798c4be6bf59980dad602930c4cd377d58e23
Hung Tran <s3715272@rmit.edu.vn>
2020-05-14 01:08:34 +1000

    edited circleci config file

--- infra/main.tf
+++ infra/main.tf
@@ -43,69 +43,69 @@ resource "local_file" "save_db_config" {
 
 ##############################################
 
-# resource "random_string" "tfstatename" {
-#   length = 6
-#   special = false
-#   upper = false
-# }
+resource "random_string" "tfstatename" {
+  length = 6
+  special = false
+  upper = false
+}
 
-# resource "aws_s3_bucket" "kops_state" {
-#   bucket        = "rmit-kops-state-${random_string.tfstatename.result}"
-#   acl           = "private"
-#   force_destroy = true
+resource "aws_s3_bucket" "kops_state" {
+  bucket        = "rmit-kops-state-${random_string.tfstatename.result}"
+  acl           = "private"
+  force_destroy = true
   
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   tags = {
-#     Name = "kops remote state"
-#   }
-# }
+  tags = {
+    Name = "kops remote state"
+  }
+}
 
-# resource "aws_ecr_repository" "app" {
-#   name                 = "app"
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "app" {
+  name                 = "app"
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = false
-#   }
-# }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-# resource "aws_s3_bucket" "tfrmstate" {
-#   bucket        = "rmit-tfstate-${random_string.tfstatename.result}"
-#   acl           = "private"
-#   force_destroy = true
+resource "aws_s3_bucket" "tfrmstate" {
+  bucket        = "rmit-tfstate-${random_string.tfstatename.result}"
+  acl           = "private"
+  force_destroy = true
 
-#   tags = {
-#     Name = "TF remote state"
-#   }
-# }
+  tags = {
+    Name = "TF remote state"
+  }
+}
 
-# resource "aws_dynamodb_table" "terraform_statelock" {
-#   name = "RMIT-locktable-${random_string.tfstatename.result}"
-#   read_capacity = 20
-#   write_capacity = 20
-#   hash_key = "LockID"
+resource "aws_dynamodb_table" "terraform_statelock" {
+  name = "RMIT-locktable-${random_string.tfstatename.result}"
+  read_capacity = 20
+  write_capacity = 20
+  hash_key = "LockID"
 
-#   attribute {
-#       name = "LockID"
-#       type = "S"
-#   }
-# }
+  attribute {
+      name = "LockID"
+      type = "S"
+  }
+}
 
-# output "state_bucket_name" {
-#   value = aws_s3_bucket.tfrmstate.bucket
-# }
+output "state_bucket_name" {
+  value = aws_s3_bucket.tfrmstate.bucket
+}
 
-# output "dynamoDb_lock_table_name" {
-#   value = aws_dynamodb_table.terraform_statelock.name
-# }
+output "dynamoDb_lock_table_name" {
+  value = aws_dynamodb_table.terraform_statelock.name
+}
 
-# output "ecr_url" {
-#   value = aws_ecr_repository.app.repository_url
-# }
+output "ecr_url" {
+  value = aws_ecr_repository.app.repository_url
+}
 
-# output "kops_state_bucket_name" {
-#   value = aws_s3_bucket.kops_state.bucket
-# }
+output "kops_state_bucket_name" {
+  value = aws_s3_bucket.kops_state.bucket
+}
